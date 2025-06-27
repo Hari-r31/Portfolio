e4a3bb33b85e11b372b34a8c374e579e000913f8
Harisai <harisai@conflowence.com>
2025-06-27 14:42:38 +0530

    git publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/", // 👈 Add this line (case-sensitive to match GitHub repo name)
   server: {
     host: "::",
     port: 8080,
