43f9599a7a96314a915a814fe13d482be646282e
Harisai <harisai@conflowence.com>
2025-06-27 14:45:44 +0530

    git publish

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom' // ✅ import this
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
